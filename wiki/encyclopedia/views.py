663f57ccf729e3b0fdaec6bebe668c001eaca28f
Bastian Elfert <bastian.elfert@outlook.com>
2020-10-21 17:53:58 +0300

    added entry pages, added links to list of entries on index page, introduced app_name and changed links accordingly

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    entry = util.get_entry(title)
+    if entry == None:
+        entry = "The requested page was not found."
+    return render(request, "encyclopedia/entry.html", {
+        "entry": entry,
+        "title": title.capitalize()
     })
 
