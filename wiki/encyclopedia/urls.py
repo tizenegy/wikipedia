607273102ea9155492b0ecee67f295d162d4ed3c
Bastian Elfert <bastian.elfert@outlook.com>
2020-10-21 20:22:20 +0300

    exchanged html form for django form, implemented POST and search page. implemented search for substrings.

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,4 +6,5 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from . import util
+from django import forms
 
+class SearchForm(forms.Form):
+    title = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder':'Search Encyclopedia'}))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": SearchForm()
     })
 
 def entry(request, title):
@@ -13,6 +17,32 @@ def entry(request, title):
         entry = "The requested page was not found."
     return render(request, "encyclopedia/entry.html", {
         "entry": entry,
-        "title": title.capitalize()
+        "title": title.capitalize(),
+        "form": SearchForm()
+    })
+
+def search(request):
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            entry = util.get_entry(title)
+            if entry == None:
+                entries = util.list_entries()
+                similar_entries = filter(lambda x: title.upper() in x.upper(), entries)
+                return render(request, "encyclopedia/search.html", {
+                "entries": similar_entries,
+                "form": SearchForm()
+            })
+            else:
+                return render(request, "encyclopedia/entry.html", {
+                    "entry": entry,
+                    "title": title.capitalize(),
+                    "form": SearchForm()
+                })
+    else:
+        return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries(),
+        "form": SearchForm()
     })
 
