bb662b59ddaf481227c6428d6c5556c4fc219ba0
Bastian Elfert <bastian.elfert@outlook.com>
2020-10-22 15:04:32 +0300

    implemented: - links on layout, - new entry - random entry - save and check if exists - new form

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("wiki/<str:title>", views.entry, name="entry"),
     path("search", views.search, name="search"),
     path("new_entry", views.new_entry, name="new_entry"),
+    path("get_random", views.get_random, name="get_random"),
+    path("edit", views.edit, name="edit"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from . import util
 from django import forms
+import random
 
 class SearchForm(forms.Form):
     title = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder':'Search Encyclopedia'}))
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder':'Enter title'}))
+    md_text = forms.CharField(label="", widget=forms.Textarea(
+        attrs={'placeholder':'Enter text in Markdown, for example:\n\n# This is a header\n1. First ordered list item\n2. Another item\n⋅⋅* Unordered sub-list.\n\nFor more go to:\nhttps://github.com/adam-p/markdown-here'}))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -12,8 +18,29 @@ def index(request):
     })
 
 def new_entry(request):
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data["md_text"]
+            title = form.cleaned_data["title"]
+            entry = util.get_entry(title)
+            if entry is None:
+                util.save_entry(title, text)
+                return render(request, "encyclopedia/entry.html", {
+                    "entry": util.get_entry(title),
+                    "title": title.capitalize(),
+                    "form": SearchForm()
+                })
+            else:
+                return render(request, "encyclopedia/new_entry.html", {
+                    "form": SearchForm(),
+                    "entry_form": NewEntryForm(),
+                    "message": "Article already exists."
+                })
+
     return render(request, "encyclopedia/new_entry.html", {
-        "form": SearchForm()
+        "form": SearchForm(),
+        "entry_form": NewEntryForm()
     })
 
 def entry(request, title):
@@ -22,7 +49,26 @@ def entry(request, title):
         entry = "The requested page was not found."
     return render(request, "encyclopedia/entry.html", {
         "entry": entry,
-        "title": title.capitalize(),
+        "title": title,
+        "form": SearchForm()
+    })
+
+def edit(request, title):
+    entry = util.get_entry(title)
+    if entry == None:
+        entry = "The requested page was not found."
+    return render(request, "encyclopedia/edit.html", {
+        "entry": entry,
+        "title": title,
+        "form": SearchForm()
+    })
+
+def get_random(request):
+    title = random.choice(util.list_entries())
+    entry = util.get_entry(title)
+    return render(request, "encyclopedia/entry.html", {
+        "entry": entry,
+        "title": title,
         "form": SearchForm()
     })
 
